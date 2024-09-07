c0131cfcf26021cd7a25fb2301b2c8e25452593e
Marc Capell <marc.capell@docker.com>
2024-09-07 18:06:47 +0200

    parser: infix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -151,3 +151,24 @@ func (pe *PrefixExpression) String() string {
 
 	return out.String()
 }
+
+type InfixExpression struct {
+	Token    token.Token // The operator token. Eg: *, - ...
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (pe *InfixExpression) expressionNode()      {}
+func (pe *InfixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Left.String())
+	out.WriteString(" " + pe.Operator + " ")
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
